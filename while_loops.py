b77ec0aaf29206272010e577664d68b0249f43f1
Temu Cats <james.ko5623@gmail.com>
2026-05-09 22:38:57 +0100

    First commit

---
+++ while_loops.py
@@ -0,0 +1,11 @@
+i = 0
+
+while i <5:
+    i = i + 1
+    print(i)
+
+while True:
+    user_input = input('Enter something >> ')
+    if user_input == '0':
+        print('We are done here')
+        break
